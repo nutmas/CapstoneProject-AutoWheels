b819f1c19709a5c7f90af23cb1c3b6ae795baf67
Diogo Silva <diogo-aos@users.noreply.github.com>
2019-04-23 20:57:05 +0100

    extracts image_raw to work with other rosbags

--- ros/src/tl_detector/image_extractor.py
+++ ros/src/tl_detector/image_extractor.py
@@ -9,9 +9,9 @@ bag = rosbag.Bag(sys.argv[1])
 out_dir = sys.argv[2]
 i = 0
 bridge = CvBridge()
-for topic, im, t in bag.read_messages(topics=['/image_color']):
+for topic, im, t in bag.read_messages(topics=['/image_raw']):
     print 'extrating ', i
     cv_image = bridge.imgmsg_to_cv2(im, "bgr8")
-    cv2.imwrite(os.path.join(out_dir, str(i) + '.jpg'), cv_image)
+    cv2.imwrite(os.path.join(out_dir, '{:05d}.jpg'.format(i)), cv_image)
     i += 1
 bag.close()
