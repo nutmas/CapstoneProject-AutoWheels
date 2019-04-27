942b319d828d9444bf22dbfc2518673559666cdf
diogoaos <diogoaos@gcx-desktop>
2019-04-27 23:24:54 +0100

    gitignore

--- ros/build/darknet_ros/darknet_ros/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /capstone/ros/src/darknet_ros/darknet_ros
-# Build directory: /capstone/ros/build/darknet_ros/darknet_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(_ctest_darknet_ros_rostest_test_object_detection.test "/capstone/ros/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/kinetic/share/catkin/cmake/test/run_tests.py" "/capstone/ros/build/test_results/darknet_ros/rostest-test_object_detection.xml" "--return-code" "/opt/ros/kinetic/share/rostest/cmake/../../../bin/rostest --pkgdir=/capstone/ros/src/darknet_ros/darknet_ros --package=darknet_ros --results-filename test_object_detection.xml --results-base-dir \"/capstone/ros/build/test_results\" /capstone/ros/src/darknet_ros/darknet_ros/test/object_detection.test ")
--- ros/build/darknet_ros_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /capstone/ros/src/darknet_ros_msgs
-# Build directory: /capstone/ros/build/darknet_ros_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
