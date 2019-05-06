bbf6c42c1531698e8dfcf0e523a796158e172258
nutmas <stephen.nutman@ntc-europe.co.uk>
2019-05-06 20:32:53 +0100

    Repo Clean Remove Build Files

--- ros/build/tl_detector/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/CarND-Capstone/ros/src/tl_detector
-# Build directory: /home/workspace/CarND-Capstone/ros/build/tl_detector
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
